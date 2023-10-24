8c6db740c5235f1cbf5cd50ab7ecd35ace08c185
tomster12 <thomas.burniston@gmail.com>
2023-10-24 19:36:07 +0100

    Fix tests

--- tests/test_protocols.py
+++ tests/test_protocols.py
@@ -5,24 +5,24 @@ from src.protocols import HTTPProtocol
 
 
 def test_http_protocol_creates_vaild_head_request():
-    http_head_request = HTTPProtocol.create_request(method="HEAD", ip="127.0.0.1")
-
-    assert http_head_request == textwrap.dedent(
+    http_head_req = HTTPProtocol.try_create_request(method="HEAD", headers={ "host": "127.0.0.1" })
+    
+    assert http_head_req.to_string() == textwrap.dedent(
         """\
         HEAD / HTTP/1.1
-        Host: 127.0.0.1
+        host: 127.0.0.1
 
     """
     )
 
 
 def test_http_protocol_creates_vaild_get_request():
-    http_get_request = HTTPProtocol.create_request(method="GET", ip="127.0.0.1", body="some test body")
+    http_get_req = HTTPProtocol.try_create_request(method="GET", headers={ "host": "127.0.0.1" }, body="some test body")
 
-    assert http_get_request == textwrap.dedent(
+    assert http_get_req.to_string() == textwrap.dedent(
         """\
         GET / HTTP/1.1
-        Host: 127.0.0.1
+        host: 127.0.0.1
 
         some test body
     """
@@ -30,5 +30,5 @@ def test_http_protocol_creates_vaild_get_request():
 
 
 def test_http_protocol_errors_on_unsupported_method():
-    with pytest.raises(NotImplementedError, match="HTTP method 'POST' is not implemented!"):
-        HTTPProtocol.create_request(method="POST", ip="127.0.0.1", body="some test body")
+    with pytest.raises(NotImplementedError, match=".*unsupported method 'POST'.*"):
+        HTTPProtocol.try_create_request(method="POST", headers={ "host": "127.0.0.1" }, body="some test body")
