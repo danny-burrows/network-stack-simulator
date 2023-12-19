c0a60ddacee6fae83834eaf47f70bae880173536
Danny Burrows <dannyburrows@protonmail.com>
2023-12-19 11:19:54 +0000

    IPPacket, IPFlags and IPOptions working all the way through!

--- src/layer_network.py
+++ src/layer_network.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from enum import IntEnum
 import random
 import struct
 
@@ -63,24 +64,228 @@ class DNSServer:
         self._records.add(record)
 
 
-#     0                   1                   2                   3
-#     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#    |Version|  IHL  |Type of Service|          Total Length         |
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#    |         Identification        |Flags|      Fragment Offset    |
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#    |  Time to Live |    Protocol   |         Header Checksum       |
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#    |                       Source Address                          |
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#    |                    Destination Address                        |
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#    |                    Options                    |    Padding    |
-#    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+@dataclass
+class IPFlags:
+    """Internal of IPPacket.
+    IP flags are 3-bits packed into a single byte."""
+
+    zero: bool = False  # Always False as specifed in RFC 791
+    dont_fragment: bool = True  # Hardcoded to True as data fragmenting is not implemented
+    more_fragments: bool = False
+
+    @classmethod
+    def from_bytes(cls, flags_bytes: bytes) -> IPFlags:
+        # Unpack packed bytes as unsigned char bitmask then construct
+        flags_bitmask = struct.unpack(">B", flags_bytes)[0]
+        return cls.from_bitmask(flags_bitmask)
+
+    @classmethod
+    def from_bitmask(cls, flags_bitmask: int) -> IPFlags:
+        # Extract first 6 bits into bools and construct
+        return cls(
+            zero=bool(flags_bitmask & (2**2)),
+            dont_fragment=bool(flags_bitmask & (2**1)),
+            more_fragments=bool(flags_bitmask & (2**0)),
+        )
+
+    def to_bytes(self) -> bytes:
+        # Convert to bitmask then directly to bytes
+        return bytes([self.to_bitmask()])
+
+    def to_bitmask_list(self) -> [int]:
+        # Convert to list of flags as bits
+        return [int(v) for v in [self.zero, self.dont_fragment, self.more_fragments]]
+
+    def to_bitmask(self) -> int:
+        # Convert to bitmask for flags
+        return sum(v * 2**i for i, v in enumerate(self.to_bitmask_list()[::-1]))
+
+    def to_string(self) -> str:
+        return self.__str__()
+
+    def to_nice_string(self) -> str:
+        return f"({', '.join(flag_name.upper() for flag_name, flag_value in vars(self).items() if flag_value)})"
+
+    def __int__(self) -> int:
+        return self.to_bitmask()
+
+    def __str__(self) -> str:
+        return "".join(str(i) for i in self.to_bitmask_list())
+
+    def __eq__(self, __value: IPFlags) -> bool:
+        return self.to_bitmask() == __value.to_bitmask()
+
+
+@dataclass
+class IPOption:
+    """Internal of IPPacket.
+    IP options are variable length, packed into words."""
+
+    class Kind(IntEnum):
+        END_OF_OPTION_LIST = 0
+        NO_OPERATION = 1
+        SECURITY = 2
+        LOOSE_SOURCE_ROUTING = 3
+        STRICT_SOURCE_ROUTING = 4
+        RECORD_ROUTE = 5
+        STREAM_ID = 6
+        INTERNET_TIMESTAMP = 7
+
+    SINGLE_BYTE_KINDS = set((Kind.END_OF_OPTION_LIST, Kind.NO_OPERATION))
+    ALL_KINDS = set(
+        (
+            Kind.END_OF_OPTION_LIST,
+            Kind.NO_OPERATION,
+            Kind.SECURITY,
+            Kind.LOOSE_SOURCE_ROUTING,
+            Kind.STRICT_SOURCE_ROUTING,
+            Kind.RECORD_ROUTE,
+            Kind.STREAM_ID,
+            Kind.INTERNET_TIMESTAMP,
+        )
+    )
+
+    @staticmethod
+    def from_bytes(option_bytes: bytes) -> IPOption:
+        (
+            option,
+            _remaining_bytes,
+        ) = IPOption.from_bytes_with_remainder(option_bytes)
+
+        return option
+
+    @staticmethod
+    def from_bytes_with_remainder(option_bytes: bytes) -> tuple[IPOption, bytes]:
+        # Extract option kind from front of option_bytes, and return along with the remainder.
+
+        # Byte 1: Option kind
+        option_kind = option_bytes[0]
+
+        if option_kind in IPOption.SINGLE_BYTE_KINDS:
+            return IPOption(kind=option_kind), option_bytes[1:]
+
+        if option_kind not in IPOption.ALL_KINDS:
+            raise NotImplementedError(f"Unsupported option kind: {option_kind}")
+
+        if len(option_bytes) <= 1:
+            raise ValueError("Option kind requires length field but no length was specified!")
+
+        # Byte 2: Length of data
+        option_length = option_bytes[1]
+
+        # Byte 3+: Option data
+        option_data = option_bytes[2:option_length]
+
+        return IPOption(kind=option_kind, data=option_data), option_bytes[option_length:]
+
+    kind: Kind
+    data: bytes = bytes()
+    len: int = field(init=False)
+
+    def __post_init__(self):
+        self.len = 1 if self.kind in IPOption.SINGLE_BYTE_KINDS else 2 + len(self.data)
+
+    def to_string(self) -> str:
+        return self.__str__()
+
+    def to_bytes(self) -> bytes:
+        if self.kind in IPOption.SINGLE_BYTE_KINDS:
+            return bytes([self.kind])
+        else:
+            return bytes([self.kind, self.len]) + self.data
+
+    def __len__(self) -> int:
+        return self.len
+
+    def __str__(self) -> str:
+        if self.kind in IPOption.SINGLE_BYTE_KINDS:
+            return f"kind={IPOption.Kind(self.kind).name}"
+        else:
+            return f"kind={IPOption.Kind(self.kind).name} len={self.len} data=0x{self.data.hex()}"
+
+
 @dataclass
 class IPPacket:
-    pass
+    """Representation of a IP packet used by IPProtocol."""
+
+    version: int
+    ihl: int
+    type_of_service: int
+    total_length: int
+    identification: int
+    flags: IPFlags
+    fragment_offset: int
+    time_to_live: int
+    protocol: int
+    header_checksum: int
+    source_address: int
+    destination_address: int
+    options: IPOption
+    data: bytes
+
+    def to_string(self) -> str:
+        return self.__str__()
+
+    def to_bytes(self) -> bytes:
+        # Pack version, ihl, and type of service
+        version_ihl_tos_bytes = struct.pack(
+            ">BBH",
+            (self.version << 4) + self.ihl,
+            self.type_of_service,
+            self.total_length,
+        )
+
+        # Pack identification, flags, and fragment offset
+        identification_flags_fragment_bytes = struct.pack(
+            ">HH",
+            self.identification,
+            self.flags.to_bitmask() << 13 + self.fragment_offset,
+        )
+
+        # Pack time to live, protocol, and header checksum
+        ttl_protocol_checksum_bytes = struct.pack(
+            ">BBH",
+            self.time_to_live,
+            self.protocol,
+            self.header_checksum,
+        )
+
+        # Pack source and destination address
+        source_dest_bytes = struct.pack(
+            ">II",
+            self.source_address,
+            self.destination_address,
+        )
+
+        # Pack options bytes up
+        options_bytes = [o.to_bytes() for o in self.options]
+
+        # Calc padding needed to ensure options are padded to the nearest 4-byte word
+        length_of_options_bytes = sum(len(o) for o in options_bytes)
+
+        # Get the nearest number of 4-byte words needed to hold all options
+        options_space_bytes = -4 * (length_of_options_bytes // -4)
+        options_padding = bytes(options_space_bytes - length_of_options_bytes)
+
+        # Construct header with all packed bits, including options and options padding
+        header = (
+            version_ihl_tos_bytes
+            + identification_flags_fragment_bytes
+            + ttl_protocol_checksum_bytes
+            + source_dest_bytes
+        )
+        for option_bytes in options_bytes:
+            header += option_bytes
+        header += options_padding
+
+        # Return final packed header + data bytes
+        return header + self.data
+
+    def __str__(self) -> str:
+        def str_list(lst):
+            return [f"{str_list(e)}" if isinstance(e, list) else str(e) for e in lst]
+
+        return "|".join(str_list(self.__dict__.values()))
 
 
 class IPProtocol:
@@ -95,12 +300,94 @@ class IPProtocol:
         return f"{(host_int >> 24) & 0xFF}.{(host_int >> 16) & 0xFF}.{(host_int >> 8) & 0xFF}.{host_int & 0xFF}"
 
     @staticmethod
-    def create_ip_packet(src_ip: str, dest_ip: str, data: bytes) -> IPPacket:
-        pass
+    def create_ip_packet(
+        src_ip: str,
+        dest_ip: str,
+        data: bytes = bytes(),
+        options: list[IPOption] = [],
+    ) -> IPPacket:
+        # Calculate the number of bytes in the header
+        MIN_IP_HEADER_BYTES = 20
+        length_of_options_bytes = sum(len(o) for o in options)
+
+        # Check options are valid and align to the nearest 4 byte word.
+        if options != [] and length_of_options_bytes % 4 != 0 and options[-1].kind != IPOption.Kind.END_OF_OPTION_LIST:
+            raise Exception(
+                "If options do not align to the nearest 4 byte word they must contain an END_OF_OPTION_LIST option!"
+            )
+
+        # Get the nearest number of 4-byte words needed to hold all options
+        options_space_bytes = -4 * (length_of_options_bytes // -4)
+
+        # Get the data offset in 4-byte words
+        ihl = (MIN_IP_HEADER_BYTES + options_space_bytes) // 4
+
+        return IPPacket(
+            version=4,  # Hardcoded to IPv4 for this simulation (also this is the version covered by RFC 791)
+            ihl=ihl,
+            type_of_service=0,  # Hardcoded to 0 (Routine) as described in RFC 791
+            total_length=20 + len(data),
+            identification=0,  # Hardcoded to 0 as unused as data fragmenting is not implemented
+            flags=IPFlags(),  # See IPFlags for default values - hardcoded for this simulation
+            fragment_offset=0,  # Hardcoded to 0 as unused as data fragmenting is not implemented
+            time_to_live=64,  # TODO: Why
+            protocol=6,  # TODO: Why
+            header_checksum=0,  # TODO
+            source_address=IPProtocol.ip_to_int(src_ip),
+            destination_address=IPProtocol.ip_to_int(dest_ip),
+            options=options,
+            data=data,
+        )
 
     @staticmethod
     def parse_ip_packet(packet_bytes: bytes) -> IPPacket:
-        pass
+        # View first 20 bytes for header and parse variables
+        (
+            version_ihl,
+            type_of_service,
+            total_length,
+            identification,
+            flags_fragment_offset,
+            time_to_live,
+            protocol,
+            header_checksum,
+            source_address,
+            destination_address,
+        ) = struct.unpack(">BBHHHBBHII", packet_bytes[:20])
+
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
+        flags = IPFlags.from_bytes(bytes([flags_fragment_offset >> 13]))
+        fragment_offset = flags_fragment_offset & 0x1FFF
+
+        options_bytes = packet_bytes[20 : ihl * 4]
+        options: list[IPOption] = []
+        while options_bytes:
+            option, options_bytes = IPOption.from_bytes_with_remainder(options_bytes)
+            options.append(option)
+
+            # Stop parsing options when reached END_OF_OPTION_LIST
+            if option.kind == IPOption.Kind.END_OF_OPTION_LIST:
+                break
+
+        data = packet_bytes[ihl * 4 :]
+
+        return IPPacket(
+            version=version,
+            ihl=ihl,
+            type_of_service=type_of_service,
+            total_length=total_length,
+            identification=identification,
+            flags=flags,
+            fragment_offset=fragment_offset,
+            time_to_live=time_to_live,
+            protocol=protocol,
+            header_checksum=header_checksum,
+            source_address=source_address,
+            destination_address=destination_address,
+            options=options,
+            data=data,
+        )
 
     def get_exam_string(ip_packet: IPPacket, note: str = "") -> str:
         pass
@@ -238,14 +525,14 @@ class NetworkLayer(Logger):
         assert route.iface in self.interfaces
         interface = self.interfaces[route.iface]
 
-        # TODO: Create packet with data
+        packet = IPProtocol.create_ip_packet(interface.bound_ip, dest_ip, data)
 
         # Send message down the interface
         self.logger.debug(
-            f"⌛ Sending data on '{interface.name}' from '{interface.bound_ip}' to '{dest_ip}' data=0x{data.hex()}..."
+            f"⌛ Sending IP packet on '{interface.name}' from '{interface.bound_ip}' to '{dest_ip}' packet={packet.to_string()}..."
         )
         self.logger.debug("⬇️ [Network->Link]")
-        interface.link.send(data)
+        interface.link.send(packet.to_bytes())
 
     def receive(self, src_ip: str) -> tuple[bytes, str]:
         # Get the interface for the source IP
@@ -258,13 +545,13 @@ class NetworkLayer(Logger):
         data = interface.link.receive()
         self.logger.debug("⬆️ [Link->Network]")
 
-        # TODO: Parse packet from data and get src IP
-        packet_src_ip = "100.100.100.100"
+        packet = IPProtocol.parse_ip_packet(data)
+        packet_src_ip = IPProtocol.int_to_ip(packet.source_address)
 
         # TODO: Validate checksums
 
         # Return final packet and IP
         self.logger.debug(
-            f"✅ Received data on '{interface.name}' from '{packet_src_ip}' to '{interface.bound_ip}' data=0x{data.hex()}"
+            f"✅ Received packet on '{interface.name}' from '{packet_src_ip}' to '{interface.bound_ip}' packet={packet.to_string()}"
         )
-        return data, packet_src_ip
+        return packet.data, packet_src_ip
