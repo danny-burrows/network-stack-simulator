2d461816aa4dd060c405ba9a3cb0ee1eb971b2a6
tomster12 <thomas.burniston@gmail.com>
2024-01-22 14:48:06 +0000

    Update protocol name

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,10 +1,10 @@
 import pytest
 
-from layer_application import HttpProtocol
+from layer_application import HTTPProtocol
 
 
 def test_create_head_request():
-    request = HttpProtocol.create_request("HEAD", "/", headers={"host": "localhost"})
+    request = HTTPProtocol.create_request("HEAD", "/", headers={"host": "localhost"})
     assert vars(request) == {
         "version": "HTTP/1.1",
         "method": "HEAD",
@@ -15,7 +15,7 @@ def test_create_head_request():
 
 
 def test_create_get_request():
-    request = HttpProtocol.create_request("GET", "/", headers={"host": "localhost"})
+    request = HTTPProtocol.create_request("GET", "/", headers={"host": "localhost"})
     assert vars(request) == {
         "version": "HTTP/1.1",
         "method": "GET",
@@ -27,16 +27,16 @@ def test_create_get_request():
 
 def test_create_head_request_fails_with_body():
     with pytest.raises(NotImplementedError, match="HTTPRequest method HEAD should not receive body='Some body!'"):
-        HttpProtocol.create_request("HEAD", "/", headers={"host": "localhost"}, body="Some body!")
+        HTTPProtocol.create_request("HEAD", "/", headers={"host": "localhost"}, body="Some body!")
 
 
 def test_create_unsupported_method_request_fails():
     with pytest.raises(NotImplementedError, match="HTTPRequest initialized with unsupported method 'POST'!"):
-        HttpProtocol.create_request("POST", "/", headers={})
+        HTTPProtocol.create_request("POST", "/", headers={})
 
 
 def test_create_response():
-    response = HttpProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
+    response = HTTPProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
     assert vars(response) == {
         "version": "HTTP/1.1",
         "status_code": "302",
@@ -47,39 +47,39 @@ def test_create_response():
 
 def test_create_response_fails_on_unsupported_status_code():
     with pytest.raises(NotImplementedError, match="HTTPResponse initialized with unsupported status code '200'!"):
-        HttpProtocol.create_response("200", headers={}, body="")
+        HTTPProtocol.create_response("200", headers={}, body="")
 
 
 def test_head_request_to_string():
-    request = HttpProtocol.create_request("HEAD", "/", headers={"host": "localhost"})
+    request = HTTPProtocol.create_request("HEAD", "/", headers={"host": "localhost"})
     assert request.to_string() == "HEAD / HTTP/1.1\nhost: localhost\n\n"
 
 
 def test_get_request_to_string():
-    request = HttpProtocol.create_request("GET", "/", headers={"host": "localhost"})
+    request = HTTPProtocol.create_request("GET", "/", headers={"host": "localhost"})
     assert request.to_string() == "GET / HTTP/1.1\nhost: localhost\n\n"
 
 
 def test_response_to_string():
-    response = HttpProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
+    response = HTTPProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
     assert response.to_string() == "HTTP/1.1 302 Found\ndate: Mon, 01 Jan 1999 00:00:00 GMT\n\nSome body!"
 
 
 def test_parse_response():
-    response = HttpProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
-    parsed_response = HttpProtocol.parse_response(response.to_string().encode("utf-8"))
+    response = HTTPProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
+    parsed_response = HTTPProtocol.parse_response(response.to_string().encode("utf-8"))
     assert parsed_response == response
 
 
 def test_parse_request():
-    request = HttpProtocol.create_request("GET", "/", headers={"host": "localhost"})
-    parsed_request = HttpProtocol.parse_request(request.to_string().encode("utf-8"))
+    request = HTTPProtocol.create_request("GET", "/", headers={"host": "localhost"})
+    parsed_request = HTTPProtocol.parse_request(request.to_string().encode("utf-8"))
     assert parsed_request == request
 
 
 def test_get_exam_string_is_correct_for_head_request():
-    request = HttpProtocol.create_request("HEAD", "/", headers={})
-    actual = HttpProtocol.get_exam_string(request)
+    request = HTTPProtocol.create_request("HEAD", "/", headers={})
+    actual = HTTPProtocol.get_exam_string(request)
     expected = """\
 ------------ BEGIN Application Layer Message ------
 RAW DATA: b'HEAD / HTTP/1.1\\n\\n'
@@ -100,8 +100,8 @@ FIELDS:
 
 
 def test_get_exam_string_is_correct_for_get_request():
-    request = HttpProtocol.create_request("GET", "/", headers={"host": "127.0.0.1"}, body="Hello, World!")
-    actual = HttpProtocol.get_exam_string(request, note="SENDING")
+    request = HTTPProtocol.create_request("GET", "/", headers={"host": "127.0.0.1"}, body="Hello, World!")
+    actual = HTTPProtocol.get_exam_string(request, note="SENDING")
     expected = """\
 ------------ SENDING Application Layer Message ----
 RAW DATA: b'GET / HTTP/1.1\\nhost: 127.0.0.1\\n\\nHello, World!'
@@ -123,8 +123,8 @@ FIELDS:
 
 
 def test_get_exam_string_is_correct_for_response():
-    response = HttpProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
-    actual = HttpProtocol.get_exam_string(response, note="RECIEVED")
+    response = HTTPProtocol.create_response("302", headers={"date": "Mon, 01 Jan 1999 00:00:00 GMT"}, body="Some body!")
+    actual = HTTPProtocol.get_exam_string(response, note="RECIEVED")
     expected = """\
 ------------ RECIEVED Application Layer Message ---
 RAW DATA: b'HTTP/1.1 302 Found\\ndate: Mon, 01 Jan 1999 00:00:00 GMT\\n\\nSome body!'
