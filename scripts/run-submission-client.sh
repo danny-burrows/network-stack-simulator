bba40986e9f3549bf39efd78041df5dfc165d3bc
Danny Burrows <dburrows@altoslabs.com>
2024-11-03 01:54:53 +0000

    Remove submission scripts

--- scripts/run-submission-client.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p submission/client/logs
-for i in $(seq 1 1 4); do python "submission/client/q${i}.py" 'client' > "submission/client/logs/q${i}.txt"; done
--- scripts/run-submission-server.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p submission/client/logs
-for i in $(seq 1 1 4); do python "submission/server/q${i}.py" 'server' > "submission/server/logs/q${i}.txt"; done
